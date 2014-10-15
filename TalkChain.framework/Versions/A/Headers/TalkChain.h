0c2a63fdf35c3e0376350bcbfb5cfcc88bcf409b
Charles Chase <cthomaschase@gmail.com>
2014-10-15 13:26:31 -0700

    TalkChain iOS 1.0.1

--- TalkChain.framework/Versions/A/Headers/TalkChain.h
+++ TalkChain.framework/Versions/A/Headers/TalkChain.h
@@ -14,6 +14,7 @@
 @optional
 
 - (void)talkChainSessionEnded;
+- (void)talkChainReceivedNotification:(NSDictionary*)notification view:(UIView*)view;
 
 @end
 
@@ -21,6 +22,25 @@
 
 @property (nonatomic, strong) id<TalkChainDelegate> delegate;
 
+/** 
+    Launch the TalkChain UI
+ 
+    @param key - App key provided at signup.
+    @param delegate - Delegate to receive callbacks.
+*/
+
 + (id)launchWithAPIKey:(NSString*)key delegate:(id<TalkChainDelegate>)delegate;
+
+/**
+    The recommended UX is to let the user manually close TalkChain via the built-in close button,
+    which ends the session, closes the UI and triggers talkChainSessionEnded. TalkChain will maintain 
+    a live connection to listen for and relay notifications, such as Direct Messages to the delegate.
+ 
+    Stop will manually close the TalkChain UI (if presented) and stop all listeners.
+ 
+    We recommend calling this only on application:didReceiveMemoryWarning.
+*/
+
++ (void)stop;
 
 @end
